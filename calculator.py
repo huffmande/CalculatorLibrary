7c889ffa02c7b8d8eb039d9e50bacf27cc5c87c3
David Huffman <huffmande@gmail.com>
2020-02-15 10:41:19 -0800

    Added a comment to the bottom of calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
 """
 Calculator library containing basic math operations.
 """
-
+""
 def add (first_term, second_term):
     return first_term + second_term
 
 def substract(first_term, second_term):
     return(first_term - second_term
+
+"""
+Add this comment at the end to practice GIT commands
+"""
