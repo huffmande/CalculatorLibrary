7f9f0ec7f4f513eaaafb6d355ae33dfcf49cdb39
David Huffman <huffmande@gmail.com>
2020-11-18 13:29:53 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def add (first_term, second_term):
     return first_term + second_term
 
 def substract(first_term, second_term):
-    return(first_term - second_term
+    return first_term - second_term
 
 """
 Add this comment at the end to practice GIT commands
